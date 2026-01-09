8e15dd1e6d673d115e9a04de3f21a47f2e7232f9
Arno Euteneuer <arno@euteneuers.de>
2026-01-10 00:30:17 +0100

    revive next-concert URL

--- scorelib/urls.py
+++ scorelib/urls.py
@@ -3,14 +3,15 @@ from . import views
 
 urlpatterns = [
     # Startseite für Musiker (nächstes Konzert)
-	path('', views.concert_detail_view, name='next_concert'),
+    path('', views.concert_detail_view, name='next_concert'),
+    path('next-concert/', views.concert_detail_view, name='next_concert'),
     
     
     # API für die Live-Suche
     path('api/search/', views.scorelib_search, name='scorelib_api_search'),
     path('concerts/', views.concert_list_view, name='concert_list'),
     path('concerts/<int:concert_id>/', views.concert_detail_view, name='concert_detail'),
-	path('archive/', views.scorelib_index, name='scorelib_index'),
+    path('archive/', views.scorelib_index, name='scorelib_index'),
     
     # abgesicherter Download
     path('download/part/<int:part_id>/', views.protected_part_download, name='protected_part_download'),
